4380cf0ce3b3356591b6dfc38fb7fe779f7393ea
iamseaniam <7222@holbertonstudents.com>
2024-04-30 10:53:57 -0500

    pycode validation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
     def ishape(matrix):
-        shapes = [ishape(x) if isinstance (x, list) else [] for x in matrix]
-        # iterates over each element x in the matrix. if x is a list, it recursivly calls (ishape(x)) to calculate its shape. if not, returns a empty list
+        shapes = [ishape(x) if isinstance(x, list) else [] for x in matrix]
+        # iterates over each element x in the matrix.
+        # if x is a list, it recursivly calls (ishape(x)) to calculate its shape.
+        # if not, returns a empty list
         shape = shapes[0]
         # assigns the first shape in shapes list to the var shape.
         shape.append(len(matrix))
-        # appends the length of current matrix to the shape list. it does it to complete shape info by adding dimension of matrix 
+        # appends the length of current matrix to the shape list.
+        # it does it to complete shape info by adding dimension of matrix
         return shape
         # returns shape of matrix
     return list(reversed(ishape(matrix)))
