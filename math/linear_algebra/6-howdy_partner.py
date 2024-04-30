deb6a7af83b372f9b7661bbe03fdfcb9d1b74001
iamseaniam <7222@holbertonstudents.com>
2024-04-30 18:25:24 -0500

    file 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""documentation"""
+
+
+def cat_arrays(arr1, arr2):
+    """Documentation"""
+    result = arr1 + arr2
+    return ("" + str(result))
---
+++ math/linear_algebra/6-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+cat_arrays = __import__('6-howdy_partner').cat_arrays
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+print(cat_arrays(arr1, arr2))
