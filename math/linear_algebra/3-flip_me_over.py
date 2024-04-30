bfd1f3d9a670a58521804850ee5f46cc332e7921
iamseaniam <7222@holbertonstudents.com>
2024-04-30 14:05:19 -0500

    pycode

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,5 +3,10 @@
 
 
 def matrix_transpose(matrix):
-    """ This is also documented """
-    return(list(map(list, zip(*matrix))))
+    """
+    Example:
+    >>> matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    >>> matrix_transpose(matrix)
+    [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    """
+    return list(map(list,zip(*matrix)))
