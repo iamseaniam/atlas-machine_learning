bad313acb17e1704a272e0310a3c7b4878885c2a
iamseaniam <7222@holbertonstudents.com>
2024-04-30 16:37:16 -0500

    d

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -5,7 +5,7 @@
 def add_matrices2D(mat1, mat2):
     """documentation"""
     if len(mat1) != len(mat2) or any(len(row1) != len(row2)
-                                    for row1, row2 in zip(mat1, mat2)):
+                                     for row1, row2 in zip(mat1, mat2)):
         return None
     result = [[mat1[i][j] + mat2[i][j]
                for j in range(len(mat1[0]))]
