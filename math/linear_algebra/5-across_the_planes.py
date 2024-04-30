60aeeb35b26243452c555bb9ca87a581a98891f2
iamseaniam <7222@holbertonstudents.com>
2024-04-30 16:08:53 -0500

    GODD DANM UIT

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""documentation"""
+
+
+def add_matrices2D(mat1, mat2):
+    """documentation"""
+    if len(mat1) != len(mat2):
+        return None
+    
+    result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+
+    return result
