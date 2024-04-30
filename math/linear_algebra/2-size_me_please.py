809d6e28e7b1ad08076cfc6d42a3150a9c613905
iamseaniam <7222@holbertonstudents.com>
2024-04-30 10:51:18 -0500

    long long, but file recurvly calls ishape to check shape of matrix by inteaing through it checking if its a list, then does more

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    print(len(matrix), len(matrix[0]))
+    def ishape(matrix):
+        shapes = [ishape(x) if isinstance (x, list) else [] for x in matrix]
+        # iterates over each element x in the matrix. if x is a list, it recursivly calls (ishape(x)) to calculate its shape. if not, returns a empty list
+        shape = shapes[0]
+        # assigns the first shape in shapes list to the var shape.
+        shape.append(len(matrix))
+        # appends the length of current matrix to the shape list. it does it to complete shape info by adding dimension of matrix 
+        return shape
+        # returns shape of matrix
+    return tuple(reversed(ishape(matrix)))
+    # revereses it to be correct format as in row col and converts it to a tuple
