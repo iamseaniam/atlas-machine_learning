db308591d8f22fda561d795e5c519c20d7d2bde4
iamseaniam <7222@holbertonstudents.com>
2024-04-30 18:38:41 -0500

    pycode

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = [] # the_middle is a maxtrix not slicing opperation
-for row in matrix: # moving through thw rows in the maxtrix
-    the_middle.append(row[2:4]) # as its moving through it takes the middle rows and uses the .append method to add to the The_middle maxtrix
+the_middle = []
+for row in matrix:
+    the_middle.append(row[2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
