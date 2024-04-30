29d70c172d60b77dc39b1ebb58f10d825afd9f77
iamseaniam <7222@holbertonstudents.com>
2024-04-30 15:38:56 -0500

    makes a lot more darn sense

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -3,13 +3,7 @@
 
 def add_arrays(arr1, arr2):
     """Documentation"""
-    if range(len(arr1) != range(len(arr2))):
+    if len(arr1) != len(arr2):
         return None
-
-    result = [[0 for _ in range(len(arr1[0]))] for _ in range(len(arr1))]
-
-    for i in range(len(arr1)):
-        for j in range(len(arr1[0])):
-            result[i][j] = arr1[i][j] + arr2[i][j]
-
-    return result
+    results = [arr1[i] + arr2[i] for i in range(len(arr1))]
+    return results
