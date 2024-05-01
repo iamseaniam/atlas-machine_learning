2288af56d0de5c5a7ed200457bf14f6b553eb336
iamseaniam <7222@holbertonstudents.com>
2024-05-01 18:15:20 -0500

    dfd

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,6 +4,11 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """muahhaha"""
+    """
+        i am also dealing with how to be lonely woah crazy.
+        feels like i am less attrative now then when i was
+        damn school is getting in the way, which is fine.
+        its more important
+    """
     New_Concatenated_Matrix = np.concatenate((mat1, mat2), axis=axis)
     return New_Concatenated_Matrix
