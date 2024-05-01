73329ff3328e89685a0a16827e2da08ebd06861d
iamseaniam <7222@holbertonstudents.com>
2024-05-01 18:06:08 -0500

    whoops did 13 i thinl

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""heloo World i am single and do not understand how to do anythin"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """muahhaha"""
+    New_Concatenated_Matrix = np.concatenate((mat1, mat2), axis = 0)
+    return New_Concatenated_Matrix
