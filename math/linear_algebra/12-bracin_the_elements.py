63a492ca1f72a87ea060d60a47a75f20305cf06c
iamseaniam <7222@holbertonstudents.com>
2024-05-01 18:23:00 -0500

    got that real big energy

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""documentation"""
+
+
+def np_elementwise(mat1, mat2):
+    """Documentation"""
+    
---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""hello documentation"""
+
+
+def np_matmul(mat1, mat2):
+    """documentation"""
+    results = np.dot(mat1,mat2)
+    return(results)
