6bc43098de67b09bdcd8a10102a0ea517340a74e
iamseaniam <7222@holbertonstudents.com>
2024-05-01 18:25:11 -0500

    pycode

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
-"""hello documentation"""
+"""documentation"""
 
 
 def np_matmul(mat1, mat2):
     """documentation"""
-    results = np.dot(mat1,mat2)
-    return(results)
+    results = np.dot(mat1, mat2)
+    return (results)
