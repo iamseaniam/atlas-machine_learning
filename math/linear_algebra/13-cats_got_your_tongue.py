56dc1bfb2718adfe64d61005da5081e6ba10ace2
iamseaniam <7222@holbertonstudents.com>
2024-05-01 18:17:24 -0500

    dsdsd

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-"""heloo World i am single and do not understand how to do anythin"""
+"""documentation"""
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """
-        i am also dealing with how to be lonely woah crazy.
-        feels like i am less attrative now then when i was
-        damn school is getting in the way, which is fine.
-        its more important
-    """
+    """documentation"""
     New_Concatenated_Matrix = np.concatenate((mat1, mat2), axis=axis)
     return New_Concatenated_Matrix
