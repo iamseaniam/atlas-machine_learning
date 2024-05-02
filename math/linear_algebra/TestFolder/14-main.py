4df8fcb39e56b839eae82278ef2a0f57a6f15a5c
iamseaniam <7222@holbertonstudents.com>
2024-05-02 10:38:43 -0500

    eeee

---
+++ math/linear_algebra/TestFolder/14-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+np_matmul = __import__('14-saddle_up').np_matmul
+
+print(np_matmul.__doc__)
+print(len(np_matmul.__doc__))
+
+mat1 = np.array([[11, 22, 33], [44, 55, 66]])
+mat2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+mat3 = np.array([[7], [8], [9]])
+# print(np_matmul(mat1, mat2))
+# print(np_matmul(mat1, mat3))
