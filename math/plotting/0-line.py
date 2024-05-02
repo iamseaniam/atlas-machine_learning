30fb9694bfaaa5ffded1be160e98122661f42c48
iamseaniam <7222@holbertonstudents.com>
2024-05-02 13:50:31 -0500

    hehehe

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""Also Documented for sissles"""
+
 
 def line():
-
+    """documented For sissles"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
-    plt.plot(x, y, color='red', linestyle='-')
+    plt.plot(x, y, color='red')
     plt.show()
