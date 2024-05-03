ac77dccc1adc35bd2324980c7c8de65781d8ab91
iamseaniam <7222@holbertonstudents.com>
2024-05-03 10:41:04 -0500

    lets see

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+"""Documentation"""
+
+
+
+def frequency():
+    """Documentation"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plotting histogram
+    plt.hist(student_grades, bins=10, edgecolor='black')
+
+    # Labels and titles
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+
+    # Display plot
+    plt.show()
