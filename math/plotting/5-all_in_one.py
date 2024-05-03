0a3fb144927fa790e6b8d256fffa8073a4a2e7cd
iamseaniam <7222@holbertonstudents.com>
2024-05-03 14:13:42 -0500

    good lotrd that was a lot of work

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -35,47 +35,85 @@ def all_in_one():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
+
+
     # setting subplots and other settings
-    fig, axs = plt.subplots(3, 2)
+    fig = plt.figure()
+    fig.subplots_adjust(left=0.2, wspace=0.6)
     fig.suptitle('All in One')
 
+
+
     # 0-line.py
-    axs[0, 0].plot(x0, y0, color='red')
-    axs[0, 0].axis([0, 10, -50, 1050])
+    ax0 = plt.subplot2grid((3,2), (0,0))
+    ax0.plot(x0, y0, color='red')
+    ax0.axis([0, 10, -50, 1050])
+    ax0.set_ylim(0, 1000)
+
+
 
     # 1-scatter.py
-    axs[0, 1].plot(x1, y1, 'o', color="magenta")
-    axs[0, 1].set_xlabel("Height (in)", fontsize="x-small")
-    axs[0, 1].set_ylabel("Weight (lbs)", fontsize="x-small")
-    axs[0, 1].set_title("Men's Height vs Weight", fontsize="x-small")
+
+    # plotting scatter graph
+    ax1 = plt.subplot2grid((3,2), (0,1))
+    s = [5]
+    ax1.scatter(x1, y1, s, color="magenta")
+
+    # Lables and titles for 1
+    ax1.set_xlabel("Height (in)", fontsize="x-small")
+    ax1.set_ylabel("Weight (lbs)", fontsize="x-small")
+    ax1.set_title("Men's Height vs Weight", fontsize="x-small")
+
+
 
     # 2-change_scale.py
-    axs[1, 0].plot(x2, y2)
-    axs[1, 0].set_xlabel("Time (years)", fontsize="x-small")
-    axs[1, 0].set_ylabel("Fraction Remaining", fontsize="x-small")
-    axs[1, 0].set_title("Exponential Decay of C-14", fontsize="x-small")
-    axs[1, 0].set_yscale("log")
-    axs[1, 0].set_xlim(0, 28650)
+    
+    # plotting line graph
+    ax2 = plt.subplot2grid((3,2), (1,0))
+    ax2.plot(x2, y2)
+    ax2.set_yscale("log")
+    ax2.set_xlim(0, 28650)
+
+    # labels and titles for 2
+    ax2.set_xlabel("Time (years)", fontsize="x-small")
+    ax2.set_ylabel("Fraction Remaining", fontsize="x-small")
+    ax2.set_title("Exponential Decay of C-14", fontsize="x-small")
+
+
 
     # 3-two.py
-    axs[1, 1].set_xlabel("Time (years)", fontsize="x-small")
-    axs[1, 1].set_ylabel("Fraction Remaining", fontsize="x-small")
-    axs[1, 1].set_title("Exponential Decay of Radioactive Elements", fontsize="x-small")
-    axs[1, 1].set_xlim(0, 20000)
-    axs[1, 1].set_ylim(0, 1)
-    axs[1, 1].plot(x3, y31, color="red", linestyle='dashed', label="C-14")
-    axs[1, 1].plot(x3, y32, label="Ra-226", color="green")
-    axs[1, 1].legend()
+
+    # plotting multi line graph
+    ax3 = plt.subplot2grid((3,2), (1,1))
+    ax3.set_xlim(0, 20000)
+    ax3.set_ylim(0, 1)
+    ax3.plot(x3, y31, color="red", linestyle='dashed', label="C-14")
+    ax3.plot(x3, y32, label="Ra-226", color="green")
+
+    # lables, title and legend for 3
+    ax3.set_xlabel("Time (years)", fontsize="x-small")
+    ax3.set_ylabel("Fraction Remaining", fontsize="x-small")
+    ax3.set_title("Exponential Decay of Radioactive Elements", fontsize="x-small")
+    ax3.legend(fontsize="x-small")
+
+
 
     # 4-frequency.py
+
+    #plotting histogram graph
+    ax4 = plt.subplot2grid((3,2), (2,0), colspan=2)
     bins = np.arange(0, 101, 10)
-    axs[2, 0].set_xlim(0, 100)
-    axs[2, 0].set_xticks(np.arange(0, 101, 10))
-    axs[2, 0].set_ylim(0, 30)
-    axs[2, 0].hist(student_grades, bins=bins, edgecolor='black')
-    axs[2, 0].set_xlabel('Grades', fontsize="x-small")
-    axs[2, 0].set_ylabel('Number of Students', fontsize="x-small")
-    axs[2, 0].set_title('Project A', fontsize="x-small")
+    ax4.set_xlim(0, 100)
+    ax4.set_xticks(np.arange(0, 101, 10))
+    ax4.set_ylim(0, 30)
+    ax4.hist(student_grades, bins=bins, edgecolor='black')
+
+    #lables and title for 4
+    ax4.set_xlabel('Grades', fontsize="x-small")
+    ax4.set_ylabel('Number of Students', fontsize="x-small")
+    ax4.set_title('Project A', fontsize="x-small")
+
+
 
     # Showing all plots
     plt.tight_layout()
