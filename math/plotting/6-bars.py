36924a62b6095396b052c3d10353b1566b2a713c
iamseaniam <7222@holbertonstudents.com>
2024-05-03 14:42:40 -0500

    why

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""documentation"""
+
 
 def bars():
+    """documentation"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
     plt.figure(figsize=(6.4, 4.8))
+
 
     # Define colors, fruits, people
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
@@ -15,13 +19,19 @@ def bars():
     # Plot stacked bars
     bottom = np.zeros(len(people))
     for i in range(len(fruit)):
-        plt.bar(people, fruit[i], bottom=bottom, color=colors[i], label=fruits[i], width=0.5)
+        plt.bar(
+            people,
+            fruit[i],
+            bottom=bottom,
+            color=colors[i],
+            label=fruits[i],
+            width=0.5)
         bottom += fruit[i]
 
     # lengend, label. title and ticks
     plt.legend()
     plt.ylabel('Quantity of Fruit')
-    plt.yticks(np.arange(0, 81, 10))
     plt.title('Number of Fruit per Person')
+    plt.yticks(np.arange(0, 81, 10))
 
     plt.show()
