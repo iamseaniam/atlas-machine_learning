805ecef3d5305a348676115545250290d554291d
iamseaniam <7222@holbertonstudents.com>
2024-05-03 14:24:58 -0500

    muanuahuaua

---
+++ math/6-bars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Define colors for each fruit
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    fruits = ['Apples', 'Bananas', 'Oranges', 'Peaches']
+    persons = ['Farrah', 'Fred', 'Felicia']
+
+    # Plot stacked bars
+    bottom = np.zeros(len(persons))
+    for i in range(len(fruit)):
+        plt.bar(persons, fruit[i], bottom=bottom, color=colors[i], label=fruits[i], width=0.5)
+        bottom += fruit[i]
+
+    #title, label, legend and ticks
+    plt.legend()
+    plt.ylabel('Quantity of Fruit')
+    plt.yticks(np.arange(0, 81, 10))
+    plt.title('Number of Fruit per Person')
+
+    plt.show()
