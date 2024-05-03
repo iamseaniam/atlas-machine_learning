f5d45b12d9ab38d9593b2238215b819123c535fa
iamseaniam <7222@holbertonstudents.com>
2024-05-03 11:05:46 -0500

    pycode, still dont know why documentation doesnt work

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 """Documentation"""
 
 
-
 def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
@@ -16,7 +15,7 @@ def frequency():
     plt.xticks(np.arange(0, 101, 10))
     plt.ylim(0, 30)
     plt.hist(student_grades, bins=bins, edgecolor='black')
-    
+
     # labels and titles
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
