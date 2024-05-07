c1710947324d2e504c98f1441d11c9a5c30d6b81
iamseaniam <7222@holbertonstudents.com>
2024-05-07 17:28:08 -0500

    wdwdw

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-"""MUHAUHAHAHAUHAHU"""
+"""MUHAUHAHAHAUHAHU i like turtles and stuff"""
 
 
 def two():
-    """muhahauhauhuahuahuahau"""
+    """muhahauhauhuahuahuahau bananan things"""
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
     t1 = 5730
@@ -13,7 +13,7 @@ def two():
     y1 = np.exp((r / t1) * x)
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
-    
+
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of Radioactive Elements")
