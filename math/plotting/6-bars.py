bf3cbd313bbdc39374c6d078ef64165ff7ea2f7b
iamseaniam <7222@holbertonstudents.com>
2024-05-07 17:26:57 -0500

    please

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-"""documentation"""
+"""documentation big balls . org"""
 
 
 def bars():
-    """documentation"""
+    """documentation big balls"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
