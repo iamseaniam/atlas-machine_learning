139db6df1b7cf9e3e11993c289dad37e1fd72c15
iamseaniam <7222@holbertonstudents.com>
2024-05-07 19:57:00 -0500

    pycode, whoops

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -11,15 +11,15 @@ def gradient():
     x = np.random.randn(2000) * 10
     y = np.random.randn(2000) * 10
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
-    
+
     scatter = plt.scatter(x, y, c=z, cmap='viridis')
-    
+
     # Create a colorbar
     plt.colorbar(scatter, label="elevation (m)", orientation="vertical")
-    
+
     # labels and title
     plt.xlabel("x coordinate (m)")
     plt.ylabel("y coordinate (m)")
     plt.title("Mountain Elevation")
-    
+
     plt.show()
