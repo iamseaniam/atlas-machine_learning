787bc1b342338d3ed8356ef0ce9bfe974586e373
iamseaniam <7222@holbertonstudents.com>
2024-05-13 13:18:28 -0500

    pycof

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """This documented throughly"""
 
+
 class Poisson:
+    """Mushrooms make some weird noises"""
     def __init__(self, data=None, lambtha=1.):
         """This class represents a Poisson distribution."""
         if data is None:
@@ -14,6 +16,6 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
-            
+
             if self.lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
