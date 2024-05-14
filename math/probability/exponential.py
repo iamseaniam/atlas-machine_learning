3b217aecd2492e8d7daa167da1eeac323afd3c11
iamseaniam <7222@holbertonstudents.com>
2024-05-14 13:14:54 -0500

    mewo

---
+++ math/probability/exponential.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Documented turtles"""
+
+
+class Exponential:
+    """This is a class, woah"""
+    def __init__(self, data=None, lambtha=1.):
+        """This is not a class, woah"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must be contain multiple values")
+            self.lambtha = sum(data) / len(data)
+            if self.lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
