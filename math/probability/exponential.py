5d5d78f4684b72061984200abd5ab6a431bc8275
iamseaniam <7222@holbertonstudents.com>
2024-05-14 20:49:23 -0500

    to muxh work

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -15,3 +15,17 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = len(data) / sum(data)
+
+    def pdf(self, x):
+        """ALSO DOCKUMENTED"""
+        if x < 0:
+            return 0
+        E =  2.7182818285
+        if x >= 0:
+            return self.lambtha * E ** (-self.lambtha * x)
+            # return ((2 * PumkinPie ** 0.5) // 1 * E - ((self.lambtha ** 2) // 2)) # close but not there
+            # return (((2 * PumkinPie) ** 0.5) // 1 * E ** ((-self.lambtha ** 2) // 2)) # smaller nummbers but not close
+            # return (((1 // 2 * PumkinPie) ** 0.5) * (E ** (-self.lambtha ** 2 // 2))) output 0
+            # return ((2 * PumkinPie ** 5) // 1 * E ** ((-self.lambtha ** 2) // 2)) numbers, but not close
+            # return (1 // (2 * PumkinPie ** 0.5) - (self.lambtha ** 2) // 2) output 0
+
