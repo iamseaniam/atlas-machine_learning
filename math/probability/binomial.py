74c7c36b9d96dc351562c51dd77eb90d0d433274
iamseaniam <7222@holbertonstudents.com>
2024-05-15 23:25:41 -0500

    hmmkm

---
+++ math/probability/binomial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python 3
+"""DOCUMENTATION"""
+
+class Binomial:
+    """DOCUMERNTST"""
+    def __init__(self, data=None, n=1, p=0.5):
+        """DOCUMENTATION"""
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not 0 < p < 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            num_successes = sum(1 for outcome in data if outcome)
+            p = num_successes / len(data)
+            n = len(data) / p
+            n = round(n)
+            p = num_successes / n
+            self.n = n
+            self.p = p
+            
