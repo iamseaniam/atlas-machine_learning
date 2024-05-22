0d1690a004d9e61ee751d0ab2b1f396db412c03e
iamseaniam <7222@holbertonstudents.com>
2024-05-22 17:24:30 -0500

    private instance

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This is documentation"""
+import numpy as np
+
+
+class Neuron:
+    """This is documented"""
+    def __init__(self, nx):
+        """This is also documented"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self._W = np.random.normal(size=(1, nx))
+        self._b = 0
+        self._A = 0
