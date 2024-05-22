888ac06a6447739f25a92bfc4d807a8be796f371
iamseaniam <7222@holbertonstudents.com>
2024-05-22 16:07:19 -0500

    instance attributes

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -11,3 +11,6 @@ class Neuron:
             TypeError("nx must be an integer")
         if nx >= 1:
             ValueError("nx must be a positive integer")
+        self.W = np.random.normal(1, nx)
+        self.b = 0
+        self.A = 0
