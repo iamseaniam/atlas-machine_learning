4b8721e2b8c11438ad98c22dbb8d8c7ae96f35e4
iamseaniam <7222@holbertonstudents.com>
2024-05-22 17:15:39 -0500

    isinstance

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -7,10 +7,10 @@ class Neuron:
     """This is documented"""
     def __init__(self,nx):
         """This is also documented"""
-        if nx != int():
-            TypeError("nx must be an integer")
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            ValueError("nx must be a positive integer")
+            raise ValueError("nx must be a positive integer")
 
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
