ea5a540191991debd201ef3d6c73a652605ce302
iamseaniam <7222@holbertonstudents.com>
2024-05-28 11:47:54 -0500

    hmhmhmh

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -64,16 +64,15 @@ class Neuron:
         """DOCUMENTATIONSJNDS"""
         if not isinstance(iterations, int):
             raise TypeError ("iterations must be an integer")
-        if iterations < 0:
+        if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
 
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-        if alpha < 0:
+        if alpha <= 0:
             raise ValueError("alpha must be positive")
 
         for _ in range(iterations):
             A = self.forward_prop(X)
-        self.gradient_descent(X, Y, A, alpha)
-
+            self.gradient_descent(X, Y, A, alpha)
         return self.evaluate(X, Y)
