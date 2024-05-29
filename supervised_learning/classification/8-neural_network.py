41d60cdc7726e3b580f9ab59303b69d4d20a3b0c
iamseaniam <7222@holbertonstudents.com>
2024-05-28 20:33:46 -0500

    pycode and documentation

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class NeuralNetwork:
+    """THIS IS DOCUMNETED"""
     def __init__(self, nx, nodes):
         """Initialize the neural network."""
         # Validate nx
@@ -15,7 +16,7 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        
+
         self.W1 = np.random.normal(size=(nodes, nx))
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
