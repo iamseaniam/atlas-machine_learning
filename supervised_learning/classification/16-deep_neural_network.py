4fb01710f555e7010de356d4e5f2772a286783e2
iamseaniam <7222@holbertonstudents.com>
2024-05-29 17:43:21 -0500

    gettterrssss

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 class DeepNeuralNetwork():
-    """Defines a deep neural network performing binary classification."""
+    """binary classification."""
     def __init__(self, nx, layers):
-        """implements a deep nerual network with many layers"""
+        """layers like a cake"""
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         elif nx < 1:
@@ -27,3 +27,18 @@ class DeepNeuralNetwork():
                 self.weights['W{}'.format(i + 1)] = w
                 dim = len(self.weights['W{}'.format(i + 1)])
                 self.weights['b{}'.format(i + 1)] = np.zeros((dim, 1))
+
+    @property
+    def L(self):
+        """getter for L"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter for cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter for weights"""
+        return self.__weights
