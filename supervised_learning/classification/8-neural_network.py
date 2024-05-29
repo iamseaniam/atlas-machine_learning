31940c5b54ff3d9ebff0b10b4b12d6ae237c64c0
iamseaniam <7222@holbertonstudents.com>
2024-05-28 20:27:30 -0500

    hmhmh

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """DOCUMENTATION"""
     def __init__(self, nx, nodes):
-        """MORE DOCUMENATION"""
-        # Error handling
+        """Initialize the neural network."""
+        # Validate nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -16,10 +15,10 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-
+        
         self.W1 = np.random.normal(size=(nodes, nx))
         self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
+        self.A1 = np.zeros((nodes, 1))
         self.W2 = np.random.normal(size=(1, nodes))
         self.b2 = np.zeros((1, 1))
-        self.A2 = 0
+        self.A2 = np.zeros((1, 1))
