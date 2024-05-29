13f5f51f57536a2ac79657a42450656c0030c57a
iamseaniam <7222@holbertonstudents.com>
2024-05-29 08:54:57 -0500

    Z2 fromula is W2 * A2

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -58,7 +58,7 @@ class NeuralNetwork:
         """DOcumentation"""
         Z1 = np.dot(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-Z1))
-        Z2 = np.dot(self.__W2, X) + self.__b2
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
         self.__A2 = 1 / (1 + np.exp(-Z2))
 
         return self.__A1, self.__A2
