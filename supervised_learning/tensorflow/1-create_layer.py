aec1412c61ba7b26ab7c0b22d5af1a03ef0d87d7
iamseaniam <7222@holbertonstudents.com>
2024-06-05 09:07:42 -0500

    updated file 1 and added test file 1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" Example Documentation """
+""" Create_layer function """
 import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
-    """ Example Documentation """
-    pass
+    """ Creates a tf layer """
+    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    return tf.layers.dense(prev, n, activation=activation,
+                            kernel_initializer=weights)
