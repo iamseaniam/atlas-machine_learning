73ea5637092d221c3ce550c7984d909ad05d4b02
iamseaniam <7222@holbertonstudents.com>
2024-06-05 12:33:30 -0500

    updated file 2 with forward propagation

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-""" Example Documentation """
+""" forward propagation """
 import tensorflow.compat.v1 as tf
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """ Example Documentation """
-    pass
+    create_layer = __import__('1-create_layer').create_layer
+    prev = x
+    for i in range(len(layer_sizes)):
+        lay = create_layer(prev, layer_sizes[i], activations[i])
+        prev = lay(prev)
+    return prev
