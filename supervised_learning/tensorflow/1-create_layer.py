e0047255d6fd5bd9730e2ec3297528d4170b6d04
iamseaniam <7222@holbertonstudents.com>
2024-06-05 09:09:12 -0500

    changed names

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,6 +5,6 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """ Creates a tf layer """
-    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     return tf.layers.dense(prev, n, activation=activation,
                             kernel_initializer=weights)
