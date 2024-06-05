039965bbdcf0709083497aec04144a3195c4f28d
iamseaniam <7222@holbertonstudents.com>
2024-06-05 13:38:34 -0500

    updated imports

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """DOCUMENTATION"""
-import numpy as np
 import tensorflow.compat.v1 as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
