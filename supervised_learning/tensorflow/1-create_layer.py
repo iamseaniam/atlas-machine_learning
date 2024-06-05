f7d902dbc955fef23bec25a38db554c8ba484679
iamseaniam <7222@holbertonstudents.com>
2024-06-05 09:13:07 -0500

    updated pycode

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,4 +7,4 @@ def create_layer(prev, n, activation):
     """ Creates a tf layer """
     weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     return tf.layers.dense(prev, n, activation=activation,
-                        kernel_initializer=weights)
+                           kernel_initializer=weights)
