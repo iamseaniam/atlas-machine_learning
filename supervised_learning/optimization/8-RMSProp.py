802c9482f14c1b4457560bebd7a1841948bd2b9a
iamseaniam <7222@holbertonstudents.com>
2024-06-12 17:24:42 -0500

    updayed

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -5,8 +5,7 @@ import tensorflow as tf
 
 def create_RMSProp_op(alpha, beta2, epsilon):
     """DOCUMENTATION"""
-    optimizer = tf.compat.v1.train.RMSPropOptimizer(learning_rate=alpha,
-                                                    decay=beta2,
-                                                    epsilon=epsilon
-                                                    )
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
+                                            rho=beta2,
+                                            epsilon=epsilon)
     return optimizer
