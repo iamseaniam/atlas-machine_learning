769db43f52ac183b6f24c446ab1301dcf7221b48
iamseaniam <7222@holbertonstudents.com>
2024-06-11 19:36:19 -0500

    meh

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 def create_momentum_op(alpha, beta1):
     """DOCUMENTATION"""
     optimizer = tf.train.MomentumOptimizer(alpha, beta1)
-    return optimizer.minimize(loss)
+    return optimizer
