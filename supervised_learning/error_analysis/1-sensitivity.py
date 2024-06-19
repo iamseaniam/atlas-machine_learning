32a0b130b8b67cc1d3ed19589a20b942c3f051d1
iamseaniam <7222@holbertonstudents.com>
2024-06-19 16:47:40 -0500

    test file and updated file number 1

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -5,4 +5,7 @@ import numpy as np
 
 def sensitivity(confusion): 
     """DOCUMNEtation"""
-    pass
+    Summing = np.sum(confusion, axis=1)
+    true_positive = np.diag(confusion)
+    false_negitive = Summing - true_positive
+    return true_positive / (true_positive + false_negitive)
