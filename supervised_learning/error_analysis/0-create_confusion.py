338f77198c1db141ac3cdb1c46ba7cf4319fa790
iamseaniam <7222@holbertonstudents.com>
2024-06-19 16:36:44 -0500

    pycode validation

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -6,9 +6,9 @@ import numpy as np
 def create_confusion_matrix(labels, logits):
     """DOCUMNEtation"""
     ConfusionMatrix = np.zeros([labels.shape[1], logits.shape[1]])
-    labels_index, logits_index = np.where(labels == 1)[1], np.where(logits == 1)[1]
+    labels_index = np.where(labels == 1)[1]
+    logits_index = np.where(logits == 1)[1]
     indexs = list(zip(labels_index, logits_index))
-    # figure out how this line works
     unique, counts = np.unique(indexs, return_counts=True, axis=0)
     ConfusionMatrix[unique[:, 0], unique[:, 1]] = counts
     return ConfusionMatrix
