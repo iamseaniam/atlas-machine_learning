f894b3adb3956683e787b25e446bbbf09faf4b5e
iamseaniam <7222@holbertonstudents.com>
2024-06-19 17:07:39 -0500

    added 4-main, updated 4-f1 and updated text files

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """Documentation"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """DOCUMNEtation"""
-    pass
+    TrueP = np.diag(confusion)
+    FalseP = np.sum(confusion, axis=0) - TrueP
+    FalseN = np.sum(confusion, axis=1) - TrueP
+    precisions = precision(confusion)
+    RecallPotion = TrueP / (TrueP + FalseN)
+    return 2/((RecallPotion ** -1) + (precisions ** -1))
