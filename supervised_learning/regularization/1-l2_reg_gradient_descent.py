b014702e1443503363c16523127c4ec5a373de10
iamseaniam <7222@holbertonstudents.com>
2024-06-19 19:46:17 -0500

    updated pycode valdation for file 1

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -7,17 +7,15 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """DOCUMENTATION"""
     m = Y.shape[1]
     A_prev = cache['A' + str(L)]
-    dA = A_prev - Y 
+    dA = A_prev - Y
 
     for i in reversed(range(1, L + 1)):
         A_prev = cache['A' + str(i - 1)] if i > 1 else cache['A0']
         W = weights['W' + str(i)]
         b = weights['b' + str(i)]
-        
         dZ = dA
         dW = (1 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        
         weights['W' + str(i)] = W - alpha * dW
         weights['b' + str(i)] = b - alpha * db
 
