99b3ef46215b6e11a483939b9f651c04d655b799
iamseaniam <7222@holbertonstudents.com>
2024-06-19 19:35:23 -0500

    main file and updsted 0-l2

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -5,4 +5,12 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """DOCUMENTATION"""
-    pass
+    l2_costatron = cost
+    l2_termialCancer = 0
+
+    for i in range(1, L + 1):
+        weight_key = 'W' + str(i)
+        l2_termialCancer += np.sum(np.square(weights[weight_key]))
+    l2_costatron += (lambtha / (2 * m)) * l2_termialCancer
+
+    return l2_costatron
