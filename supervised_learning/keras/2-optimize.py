7476ea2de1e231225718a6166922d964aafd0b95
iamseaniam <7222@holbertonstudents.com>
2024-06-25 14:52:55 -0500

    file 2-optimsim

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""THIS IS DOCUmentation"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Sets up Adam optimization for a keras model with categorical crossentropy loss
+    and accuracy metrics.
+    """
+    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=optimizer,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy']
+                    )
