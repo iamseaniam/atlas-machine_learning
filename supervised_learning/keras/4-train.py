7ae67fce49a2d92e6a64a11ef42fb7a363d3c4b9
iamseaniam <7222@holbertonstudents.com>
2024-06-25 15:43:39 -0500

    Updated pycode validation

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,7 +3,14 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network,
+                data,
+                labels,
+                batch_size,
+                epochs,
+                verbose=True,
+                shuffle=False
+                ):
     """
     Trains a model using mini-batch gradient descent.
     """
