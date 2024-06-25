321fe32328b6464ce759115ad6969cd5fbfd2ffb
iamseaniam <7222@holbertonstudents.com>
2024-06-25 10:41:07 -0500

    pycode validation

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""documentation"""
 import tensorflow.keras as K
 
 
@@ -8,15 +9,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     inputs = K.Input(shape=(nx,))
     x = inputs
-    
+
     for i in range(len(layers)):
         x = K.layers.Dense(
-            layers[i], 
-            activation=activations[i], 
+            layers[i],
+            activation=activations[i],
             kernel_regularizer=K.regularizers.l2(lambtha)
         )(x)
         if i < len(layers) - 1:
             x = K.layers.Dropout(1 - keep_prob)(x)
-    
+
     model = K.Model(inputs=inputs, outputs=x)
     return model
