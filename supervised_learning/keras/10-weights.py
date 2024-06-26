8466234fa6f00e6b65ca6391667b37830c832a14
iamseaniam <7222@holbertonstudents.com>
2024-06-26 13:50:00 -0500

    put bare basics into 10

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""DOCUMENTATION"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='keras'):
+    pass
+
+def load_weights(network, filename):
+    pass
