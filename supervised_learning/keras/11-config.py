8e7f13e955ef480a40d932de99aadc13f28e1880
iamseaniam <7222@holbertonstudents.com>
2024-06-26 15:18:11 -0500

    pycode validation

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -4,11 +4,14 @@ import tensorflow.keras as K
 
 
 def save_config(network, filename):
+    """DONT forget documentation"""
     config = network.to_json()
     with open(filename, 'w') as json_file:
         json_file.write(config)
 
+
 def load_config(filename):
+    """Documentation"""
     with open(filename, "r") as f:
         model = f.read()
     return K.models.model_from_json(model)
