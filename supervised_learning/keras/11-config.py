f2290ecb90e41b84e9bf17fc3e49cd7ba915c3f2
iamseaniam <7222@holbertonstudents.com>
2024-06-26 15:05:47 -0500

    updated 11

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -4,7 +4,11 @@ import tensorflow.keras as K
 
 
 def save_config(network, filename):
-    pass
+    config = network.to_json()
+    with open(filename, 'w') as json_file:
+        json_file.write(config)
 
 def load_config(filename):
-    pass
+    with open(filename, "r") as f:
+        model = f.read()
+    return K.models.model_from_json(model)
