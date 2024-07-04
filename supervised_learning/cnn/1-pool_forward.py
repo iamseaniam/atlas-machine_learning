260821caf4f4d2f2ff8aa21a7cfd136fb35e5fca
iamseaniam <7222@holbertonstudents.com>
2024-07-03 23:57:17 -0500

    updated 1-2

---
+++ supervised_learning/cnn/1-pool_forward.py
@@ -0,0 +1,36 @@
+#!/user/bin/env python3
+"""documentation"""
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+    Performs forward propagation over a pooling layer of a neural network.
+    """
+
+    (m, h_prev, w_prev, c_prev) = A_prev.shape
+    (kh, kw) = kernel_shape
+    (sh, sw) = stride
+
+    h_new = (h_prev - kh) // sh + 1
+    w_new = (w_prev - kw) // sw + 1
+
+    A = np.zeros((m, h_new, w_new, c_prev))
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_prev):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+                    
+                    A_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                    
+                    if mode == 'max':
+                        A[i, h, w, c] = np.max(A_slice)
+                    elif mode == 'avg':
+                        A[i, h, w, c] = np.mean(A_slice)
+
+    return A
--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -1,7 +1,56 @@
 #!/usr/bin/env python3
 """documentation"""
+import numpy as np
+import tensorflow.compat.v1 as tf
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """documentation"""
-    pass
+    """
+    Performs back propagation over a convolutional layer of a neural network.
+    """
+
+    (m, h_prev, w_prev, c_prev) = A_prev.shape
+    (kh, kw, c_prev, c_new) = W.shape
+    (sh, sw) = stride
+    (m, h_new, w_new, c_new) = dZ.shape
+
+    if padding == "same":
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
+    elif padding == "valid":
+        ph, pw = 0, 0
+    else:
+        raise ValueError("Padding must be either 'same' or 'valid'")
+
+    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant', constant_values=(0, 0))
+    dA_prev_pad = np.pad(np.zeros_like(A_prev), ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant', constant_values=(0, 0))
+
+    dW = np.zeros_like(W)
+    db = np.zeros_like(b)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+                    
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+                    
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[..., c] * dZ[i, h, w, c]
+                    dW[..., c] += a_slice * dZ[i, h, w, c]
+                    db[..., c] += dZ[i, h, w, c]
+        dA_prev_pad[i] = da_prev_pad
+
+    if padding == "same":
+        dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+    elif padding == "valid":
+        dA_prev = dA_prev_pad
+
+    return dA_prev, dW, db
+
