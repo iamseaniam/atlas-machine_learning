7e4792d3eb1879cc28f733eadf205f814c81f9cc
iamseaniam <7222@holbertonstudents.com>
2024-07-10 16:55:11 -0500

    hmm i cant match

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -7,4 +7,44 @@ projection_block = __import__('3-projection_block').projection_block
 
 def resnet50():
     """Documentatinh"""
-    pass
+    he_normal = K.initializers.HeNormal(seed=0)
+    input = K.Input(shape=(224, 224, 3))
+
+    X = K.layers.Conv2D(
+        filters=64,
+        kernel_size=(7, 7),
+        strides=(2, 2),
+        padding='same',
+        kernel_initializer=he_normal
+    )(input)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(X)
+
+    X = projection_block(X, filters=[64, 64, 256], s=1)
+    X = identity_block(X, filters=[64, 64, 256])
+    X = identity_block(X, filters=[64, 64, 256])
+
+    X = projection_block(X, filters=[128, 128, 512], s=2)
+    X = identity_block(X, filters=[128, 128, 512])
+    X = identity_block(X, filters=[128, 128, 512])
+    X = identity_block(X, filters=[128, 128, 512])
+
+    X = projection_block(X, filters=[256, 256, 1024], s=2)
+    X = identity_block(X, filters=[256, 256, 1024])
+    X = identity_block(X, filters=[256, 256, 1024])
+    X = identity_block(X, filters=[256, 256, 1024])
+    X = identity_block(X, filters=[256, 256, 1024])
+    X = identity_block(X, filters=[256, 256, 1024])
+
+    X = projection_block(X, filters=[512, 512, 2048], s=2)
+    X = identity_block(X, filters=[512, 512, 2048])
+    X = identity_block(X, filters=[512, 512, 2048])
+
+    X = K.layers.AveragePooling2D(pool_size=(7, 7), padding='same')(X)
+
+    X = K.layers.Dense(units=1000, activation='softmax', kernel_initializer=he_normal)(X)
+
+    model = K.models.Model(inputs=input, outputs=X)
+
+    return model
