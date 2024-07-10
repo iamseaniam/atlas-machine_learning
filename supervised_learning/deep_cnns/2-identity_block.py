a72428f8414b43b2e89ef26a4fbede9e0e6b4841
iamseaniam <7222@holbertonstudents.com>
2024-07-10 15:52:55 -0500

    updated 2 and 3

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -4,5 +4,52 @@ from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
-    """Documentatinh"""
-    pass
+    """
+    * A_prev - is the output from the previous layer
+    * filters - is a tuple or list containing F11, F3, F12, respectively:
+    * F11 - is the number of filters in the first 1x1 convolution
+    * F3 - is the number of filters in the 3x3 convolution
+    * F12 - is the number of filters in the second 1x1 convolution
+    * All convolutions inside the block should be followed by batch normalization along the channels axis and a rectified linear activation (ReLU), respectively.
+    * All weights should use he normal initialization
+    * The seed for the he_normal initializer should be set to zero
+    * Returns: the activated output of the identity block
+    """
+    init = K.initializers.he_normal()
+    F11, F3, F12, = filters
+
+    conv11 = K.layers.Conv2D(
+        filters=F11,
+        kernel_size=(1, 1),
+        strides=1,
+        padding='same',
+        kernel_initializer=init
+    )(A_prev)
+
+    Batch11 = K.layers.BatchNormalization(axis=3)(conv11)
+    Relu11 = K.layers.Activation('relu')(Batch11)
+
+
+
+    conv3 = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer=init
+    )(A_prev)
+
+    conv12 = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=(1, 1),
+        padding='same',
+
+        kernel_initializer=init
+    )(A_prev)
+
+    concatenate = K.layers.Concatenate()([
+        conv11,
+        conv3,
+        conv12
+    ])
+
+    return (concatenate + A_prev)
--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -5,4 +5,46 @@ from tensorflow import keras as K
 
 def projection_block(A_prev, filters, s=2):
     """Documentatinh"""
-    pass
+    F11, F3, F12 = filters
+    he_normal = K.initializers.HeNormal(seed=0)
+
+    main_path = K.layers.Conv2D(
+        filters=F11,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        padding='same',
+        kernel_initializer=he_normal
+    )(A_prev)
+    main_path = K.layers.BatchNormalization(axis=3)(main_path)
+    main_path = K.layers.Activation('relu')(main_path)
+
+    main_path = K.layers.Conv2D(
+        filters=F3,
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer=he_normal
+    )(main_path)
+    main_path = K.layers.BatchNormalization(axis=3)(main_path)
+    main_path = K.layers.Activation('relu')(main_path)
+
+    main_path = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=(1, 1),
+        padding='same',
+        kernel_initializer=he_normal
+    )(main_path)
+    main_path = K.layers.BatchNormalization(axis=3)(main_path)
+
+    shortcut = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        padding='same',
+        kernel_initializer=he_normal
+    )(A_prev)
+    shortcut = K.layers.BatchNormalization(axis=3)(shortcut)
+
+    output = K.layers.Add()([main_path, shortcut])
+    output = K.layers.Activation('relu')(output)
+
+    return output
