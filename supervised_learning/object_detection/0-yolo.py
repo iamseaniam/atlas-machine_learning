c59f7827063e10d1638c4464cee284145a279b06
iamseaniam <7222@holbertonstudents.com>
2024-08-06 14:33:20 -0500

    test

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """Example documentation"""
+import numpy as np
+import tensorflow as tf
+
 
 class Yolo:
     """The YOLOv3 Class documentation"""
-
-
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Example documentation"""
-        pass
+        self.model = tf.keras.models.load_model(model_path)
+        
+        with open(classes_path, 'r') as file:
+            self.class_names = [line.strip() for line in file.readlines()]
+        
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
