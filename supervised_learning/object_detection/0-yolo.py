79c1dcd00e53877cb4c01a9b4469dcea64fdf7a1
iamseaniam <7222@holbertonstudents.com>
2024-08-06 13:14:24 -0500

    added suppor files and updated file 0 with yolo class

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Example documentation"""
 
+class Yolo:
+    """The YOLOv3 Class documentation"""
 
-def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-    """Example documentation"""
-    pass
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Example documentation"""
+        pass
