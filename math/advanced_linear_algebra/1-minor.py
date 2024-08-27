ee24116d968ecf4929c7899600a6388862f27efa
iamseaniam <7222@holbertonstudents.com>
2024-08-27 17:02:50 -0500

    updated file 1

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -3,5 +3,24 @@
 
 
 def minor(matrix):
-    """DOCUMENtation"""
-    pass
+    """Calculates the minor matrix of a matrix."""
+    if not isinstance(matrix,
+                      list) or not all(isinstance(row,
+                                                  list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 0 or not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    size = len(matrix)
+    minor_matrix = []
+
+    for i in range(size):
+        row_minors = []
+        for j in range(size):
+            sub_matrix = [row[:j] + row[j+1:] for idx,
+                          row in enumerate(matrix) if idx != i]
+            row_minors.append(sub_matrix)
+        minor_matrix.append(row_minors)
+
+    return minor_matrix
