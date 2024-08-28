5bd5aa0eefa98ac55b214a03f255859817cee91c
iamseaniam <7222@holbertonstudents.com>
2024-08-28 09:30:51 -0500

    final update for 1-minor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -3,24 +3,33 @@
 
 
 def minor(matrix):
-    """Calculates the minor matrix of a matrix."""
     if not isinstance(matrix,
                       list) or not all(isinstance(row,
                                                   list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-
-    if len(matrix) == 0 or not all(len(row) == len(matrix) for row in matrix):
+    if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
-    size = len(matrix)
+    def determinant(SubM):
+        """Helper function to calculate the determinant of a matrix"""
+        if len(SubM) == 1:
+            return SubM[0][0]
+        if len(SubM) == 2:
+            return SubM[0][0] * SubM[1][1] - SubM[0][1] * SubM[1][0]
+        
+        det = 0
+        for c in range(len(SubM)):
+            minor = [[SubM[i][j] for j in range(len(SubM)) if j != c] for i in range(1, len(SubM))]
+            det += ((-1) ** c) * SubM[0][c] * determinant(minor)
+        return det
+
     minor_matrix = []
-
+    size = len(matrix)
     for i in range(size):
-        row_minors = []
+        minor_row = []
         for j in range(size):
-            sub_matrix = [row[:j] + row[j+1:] for idx,
-                          row in enumerate(matrix) if idx != i]
-            row_minors.append(sub_matrix)
-        minor_matrix.append(row_minors)
-
+            SubM = [[matrix[row][col] for col in range(size) if col != j] for row in range(size) if row != i]
+            minor_row.append(determinant(SubM))
+        minor_matrix.append(minor_row)
+    
     return minor_matrix
