03503137da1a8f8e61955b34e5241b2656338791
iamseaniam <7222@holbertonstudents.com>
2024-08-28 13:22:46 -0500

    meh

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """Module for matrix definiteness checking."""
-
 import numpy as np
 
 
 def definiteness(matrix):
-    """
-    Check if the input is a non-empty square matrix.
-    """
+    """ Check if the input is a non-empty square matrix. """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
 
