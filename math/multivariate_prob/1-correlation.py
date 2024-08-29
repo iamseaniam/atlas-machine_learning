39499daa7f4b00bfa368800fb4f3e33a752eb050
iamseaniam <7222@holbertonstudents.com>
2024-08-29 14:06:53 -0500

    updated error handling

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,5 +7,5 @@ def correlation(C):
     """Documentation"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if not np.shape(C, "(d, d)"):
+    if not C.ndim == 2:
         raise ValueError("C must be a 2D square matrix")
