182acd65017a476cfe45b499160f7489c26cef87
iamseaniam <7222@holbertonstudents.com>
2024-08-29 14:15:01 -0500

    testinggggggggg

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -5,9 +5,10 @@ import numpy as np
 
 def correlation(C):
     """Documentation"""
+    d = 0
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if C.shape != (d, d):
         raise ValueError("C must be a 2D square matrix")
 
-    d = 0
+
