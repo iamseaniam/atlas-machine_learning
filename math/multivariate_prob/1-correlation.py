50bd1dc099ba5fc7905592eab304e84fb6dbd905
iamseaniam <7222@holbertonstudents.com>
2024-08-29 14:11:19 -0500

    updated errrrrrror handlig angain becasue i do not get it muaushdioajm

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,5 +7,7 @@ def correlation(C):
     """Documentation"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if not isinstance(C, np.ndarray) or C.ndim == 2:
+    if C.shape != (d, d):
         raise ValueError("C must be a 2D square matrix")
+
+    d = 0
