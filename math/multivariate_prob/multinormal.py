a0f3325822597d47b3beb890cc697b07d147c3a9
iamseaniam <7222@holbertonstudents.com>
2024-08-29 14:18:46 -0500

    updated handling

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -5,12 +5,11 @@ import numpy as np
 
 def __init__(self, data):
     """More fake documentation"""
+    n = 0
     if not isinstance(data, np.ndarray) or data.nidm != 2:
         raise TypeError("data must be a 2D numpy.ndarray")
     if n < 2:
         raise ValueError("X must contain multiple data points")
-
-    n = 0
 
 def pdf(self, x):
     """Documentation"""
