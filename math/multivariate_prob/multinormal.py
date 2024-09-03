c1c588d7f2a1148658dfd3914b85b2bbac9fe2c7
iamseaniam <7222@holbertonstudents.com>
2024-09-02 19:23:05 -0500

    pycode and documentation

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -4,7 +4,9 @@ import numpy as np
 
 
 class MultiNormal:
+    """documetation"""
     def __init__(self, data):
+        """documentation"""
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
@@ -17,6 +19,7 @@ class MultiNormal:
 
         centered_data = data - self.mean
         self.cov = (centered_data @ centered_data.T) / (n - 1)
+
 
 def pdf(self, x):
     """Documentation"""
