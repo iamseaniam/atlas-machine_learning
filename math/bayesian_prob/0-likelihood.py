a40a3641325dddc2604eff1219e298cfb5c24318
iamseaniam <7222@holbertonstudents.com>
2024-09-04 19:24:24 -0500

    updatede pycode and Cap the p in P

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -6,16 +6,26 @@ import numpy as np
 def likelihood(x, n, P):
     """documentation"""
     if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a postive integer")
+        raise ValueError(
+            "n must be a postive integer"
+            )
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+            )
 
     if x > n:
-        raise ValueError("x cannot be greater than n")
+        raise ValueError(
+            "x cannot be greater than n"
+            )
 
     if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
+        raise TypeError(
+            "P must be a 1D numpy.ndarray"
+            )
 
-    if not np.all((0 <= P) & (p <=1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
+    if not np.all((0 <= P) & (P <=1)):
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+            )
