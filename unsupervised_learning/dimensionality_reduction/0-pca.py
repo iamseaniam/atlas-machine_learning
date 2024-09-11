6e3dc7156319072d027b83d63a4f7d97ebf94adb
iamseaniam <7222@holbertonstudents.com>
2024-09-11 10:17:07 -0500

    updated file 0 to do PCA

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -5,4 +5,17 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Documentation"""
-    pass
+    cov_matrix = np.cov(X, rowvar=False)
+
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+    sorted_idx = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_idx]
+    sorted_eigenvectors = eigenvectors[:, sorted_idx]
+
+    cum_var_explained = np.cumsum(sorted_eigenvectors) / np.sum(sorted_eigenvalues)
+
+    num_components = np.searchsorted(cum_var_explained, var) + 1
+    W = sorted_eigenvectors[:, :num_components]
+
+    return W
