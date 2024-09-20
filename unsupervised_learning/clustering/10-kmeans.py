ee6f8f15b276c9b231f32972b83946a195ad4f72
iamseaniam <7222@holbertonstudents.com>
2024-09-20 09:18:56 -0500

    no numpy

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 """Docmentation"""
-import numpy as np
 import sklearn.cluster as skcls
 
 
 def kmeans(X, k):
     """Performs K-means on a dataset."""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None
-    if not isinstance(k, int) or k <= 0:
-        return None, None
-
     centroid, label, inertia = skcls.k_means(X, k)
     return centroid, label
