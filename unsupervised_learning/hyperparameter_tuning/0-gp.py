f1d65b6a3b60e0d26fdd084d15a2542b98dbffce
iamseaniam <7222@holbertonstudents.com>
2024-10-02 13:24:14 -0500

    file 0 updated

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 3
+"""documentation"""
+import numpy as np
+
+
+class GaussianProcess:
+    """Documentation"""
+    
+    
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Documentation"""        
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """documentation"""
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(
+            X2**2,  1) - 2 * np.dot(X1, X2.T)
+
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
