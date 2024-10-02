b2eed4607b29c5a9d54c5b4793209e0168b5ee11
iamseaniam <7222@holbertonstudents.com>
2024-10-02 13:28:05 -0500

    updated pycode again

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -6,15 +6,16 @@ import numpy as np
 class GaussianProcess:
     """Documentation"""
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """Documentation"""        
+        """Documentation""" 
         self.X = X_init
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
         self.K = self.kernel(self.X, self.X)
+
     def kernel(self, X1, X2):
         """documentation"""
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(
-            X2**2,  1) - 2 * np.dot(X1, X2.T)
+            X2**2, 1) - 2 * np.dot(X1, X2.T)
 
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
