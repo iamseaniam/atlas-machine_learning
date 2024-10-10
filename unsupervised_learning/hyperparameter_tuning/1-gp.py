d212c8b19155e040fe37d09859feca1fbc73b451
iamseaniam <7222@holbertonstudents.com>
2024-10-09 19:43:13 -0500

    updated file 1

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -3,6 +3,30 @@
 import numpy as np
 
 
-def predict(self, X_s):
+class GaussianProcess:
     """Documentation"""
-    pass
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Documentation"""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """documentation"""
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(
+            X2**2, 1) - 2 * np.dot(X1, X2.T)
+
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """Documentation"""
+        K_s = self.kernel(X_s, self.X)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+        mu_s = K_s.dot(K_inv).dot(self.Y).flatten()
+        sigma_s = K_ss - K_s.dot(K_inv).dot(K_s.T)
+        sigma_s = np.diag(sigma_s)
+
+        return mu_s, sigma_s
---
+++ unsupervised_learning/hyperparameter_tuning/test.py
@@ -0,0 +1,5 @@
+# from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+
+print(tf.__version__)
