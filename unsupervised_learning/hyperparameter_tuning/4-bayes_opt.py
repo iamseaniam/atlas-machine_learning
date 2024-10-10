cf8ffb4b87c6c12f7cb3cf1fa34ee2b32bc7b66f
iamseaniam <7222@holbertonstudents.com>
2024-10-09 20:02:08 -0500

    oh my

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -2,8 +2,38 @@
 """Python documentation"""
 import numpy as np
 from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
 
 
-def acquisition(self):
+class BayesianOptimization:
     """Documentation"""
-    pass
+    def __init__(self, f, X_init, Y_init,
+                 bounds, ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
+        """
+        Class constructor for Bayesian Optimization on a noiseless 1D Gaussian
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """
+        Calculates the next best sample location using Expected Improvement (EI)
+        """
+        mu_s, sigma_s = self.gp.predict(self.X_s)
+        sigma_s = np.maximum(sigma_s, 1e-9)
+        if self.minimize:
+            mu_opt = np.min(self.gp.Y)
+        else:
+            mu_opt = np.max(self.gp.Y)
+        with np.errstate(divide='warn'):
+            improvement = mu_opt - mu_s - self.xsi if self.minimize else mu_s - mu_opt - self.xsi
+            Z = improvement / sigma_s
+            EI = improvement * norm.cdf(Z) + sigma_s * norm.pdf(Z)
+
+        X_next = self.X_s[np.argmax(EI)].reshape(-1, 1)
+
+        return X_next, EI
