ead0d0e5314b9ee3eb82f176c586de6f5e4930ad
iamseaniam <7222@holbertonstudents.com>
2024-10-22 18:53:22 -0500

    Re formated code and added good documentation for once

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -3,40 +3,44 @@
 import tensorflow.keras as keras
 
 
-def build_encoder(input_dim, hidden_layer, latent_dims):
-    """Documentation"""
-    input = keras.layers.Input(shape=(input_dims,))
-    x = inputs
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """autoencoder
 
-    # hl: Add hidden layers
-    for nodes in hidden_layer:
+    Args:
+        input_dims (integer): dimensions of model input
+        hidden_layers (list): num of nodes for each hidden layer in encoder
+        latent_dims ( integer): dimensions of the latent space representation
+
+    Returns:
+        encoder, decoder, auto
+    """
+
+    encoder_input = keras.layers.Input(shape=(input_dims,))
+    x = encoder_input
+
+    for nodes in hidden_layers:
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
-    # hl: latent space 
-    latent = keras.layers.Dense(latent_dims, activations='relu')(x)
+    latent_space = keras.layers.Dense(latent_dims, activation='relu')(x)
 
-    encoder = keras.models.Model(inputs, latent, name='encoder')
-    return encoder
+    encoder = keras.models.Model(encoder_input, latent_space, name='encoder')
 
-def build_decoder(latent_dims, hidden_layers, input_dims):
-    """Documentation"""
-    latent_inputs = keras.layers.Input(shape=(latent_dims,))
-    x = latent_inputs
+    decoder_input = keras.layers.Input(shape=(latent_dims,))
+    x = decoder_input
 
-    # hl: Reverse the hidden layers for the decoder
     for nodes in reversed(hidden_layers):
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
-    # hl: Output layer should match the original input dimensions
-    outputs = keras.layers.Dense(input_dims, activations='sigmoid')(x)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    decoder = keras.models.Model(latent_inputs, outputs, name='decoder')
-    return decoder
+    decoder = keras.models.Model(decoder_input, decoder_output, name='decoder')
 
-def autoencoder(input_dims, hidden_layers, latent_dims):
-    """Documentation"""
-    auto_input = encoder.input
-    auto_output = decoder(encoder(auto_input))
+    auto_input = keras.layers.Input(shape=(input_dims,))
+    encoded = encoder(auto_input)
+    decoded = decoder(encoded)
 
-    auto = models.Model(auto_input, auto_output, name='autoencoder')
-    return auto
+    auto = keras.models.Model(auto_input, decoded, name='autoencoder')
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
