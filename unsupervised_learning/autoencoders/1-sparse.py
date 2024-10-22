62e86f2746297824898f9c193a525c17c3b7d6a3
iamseaniam <7222@holbertonstudents.com>
2024-10-22 15:48:19 -0500

    layout for files

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Documentation"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Documentation"""
+    pass
---
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Documentation"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, filters, latent_dims):
+    """Documentation"""
+    pass
