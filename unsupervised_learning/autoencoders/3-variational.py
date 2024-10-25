b248c9c1ccf210c09a1a5be53b6488699ac19f38
iamseaniam <7222@holbertonstudents.com>
2024-10-25 09:02:21 -0500

    checking pycode

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -21,9 +21,11 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     z_mean = keras.layers.Dense(latent_dims)(x)
     z_log_sigma = keras.layers.Dense(latent_dims)(x)
 
-    z = keras.layers.Lambda(sampling, output_shape=(latent_dims,))([z_mean, z_log_sigma])
+    z = keras.layers.Lambda(sampling,
+                            output_shape=(latent_dims,))([z_mean, z_log_sigma])
 
-    encoder = keras.models.Model(encoder_input, [z, z_mean, z_log_sigma], name="encoder")
+    encoder = keras.models.Model(encoder_input,
+                                 [z, z_mean, z_log_sigma], name="encoder")
 
     decoder_input = keras.layers.Input(shape=(latent_dims,))
     x = decoder_input
@@ -31,18 +33,22 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         x = keras.layers.Dense(nodes, activation='relu')(x)
     decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    decoder = keras.models.Model(decoder_input, decoder_output, name="decoder")
+    decoder = keras.models.Model(decoder_input,
+                                 decoder_output, name="decoder")
 
     autoencoder_input = keras.layers.Input(shape=(input_dims,))
     z, z_mean, z_log_sigma = encoder(autoencoder_input)
     reconstructed = decoder(z)
-    auto = keras.models.Model(autoencoder_input, reconstructed, name="autoencoder")
+    auto = keras.models.Model(autoencoder_input,
+                              reconstructed, name="autoencoder")
 
     def vae_loss(x, reconstructed_x):
         """Documentation"""
-        reconstruction_loss = keras.losses.binary_crossentropy(x, reconstructed_x)
+        reconstruction_loss = keras.losses.binary_crossentropy(
+            x, reconstructed_x)
         reconstruction_loss *= input_dims
-        kl_loss = 1 + z_log_sigma - keras.backend.square(z_mean) - keras.backend.exp(z_log_sigma)
+        kl_loss = 1 + z_log_sigma - keras.backend.square(
+            z_mean) - keras.backend.exp(z_log_sigma)
         kl_loss = keras.backend.sum(kl_loss, axis=-1)
         kl_loss *= -0.5
         return keras.backend.mean(reconstruction_loss + kl_loss)
