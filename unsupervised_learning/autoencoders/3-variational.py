024e6a28e12ab866076caf122fd9d43875a56f92
iamseaniam <7222@holbertonstudents.com>
2024-10-25 08:59:13 -0500

    EEEEEEEEEEEEEEE

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -3,42 +3,50 @@
 import tensorflow.keras as keras
 
 
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian."""
+    z_mean, z_log_sigma = args
+    batch = keras.backend.shape(z_mean)[0]
+    dim = keras.backend.int_shape(z_mean)[1]
+    epsilon = keras.backend.random_normal(shape=(batch, dim))
+    return z_mean + keras.backend.exp(0.5 * z_log_sigma) * epsilon
+
 def autoencoder(input_dims, hidden_layers, latent_dims):
-    """Doumentation"""
-    inputs = tensorflow.keras.Input(shape=(input_dims,))
-    x = inputs
+    """Documentation"""
+    encoder_input = keras.layers.Input(shape=(input_dims,))
+    x = encoder_input
     for nodes in hidden_layers:
-        x = tensorflow.keras.layers.Dense(nodes, activation='relu')(x)
+        x = keras.layers.Dense(nodes, activation='relu')(x)
 
-    z_mean = tensorflow.keras.layers.Dense(latent_dims)(x)
-    z_log_var = tensorflow.keras.layers.Dense(latent_dims)(x)
+    z_mean = keras.layers.Dense(latent_dims)(x)
+    z_log_sigma = keras.layers.Dense(latent_dims)(x)
 
-    def sampling(args):
-        z_mean, z_log_var = args
-        epsilon = tensorflow.keras.backend.random_normal(shape=tensorflow.keras.backend.shape(z_mean))
-        return z_mean + tensorflow.keras.backend.exp(0.5 * z_log_var) * epsilon
+    z = keras.layers.Lambda(sampling, output_shape=(latent_dims,))([z_mean, z_log_sigma])
 
-    z = tensorflow.keras.layers.Lambda(sampling)([z_mean, z_log_var])
+    encoder = keras.models.Model(encoder_input, [z, z_mean, z_log_sigma], name="encoder")
 
-    encoder = tensorflow.keras.Model(inputs, [z, z_mean, z_log_var], name="encoder")
-
-    latent_inputs = tensorflow.keras.Input(shape=(latent_dims,))
-    x = latent_inputs
+    decoder_input = keras.layers.Input(shape=(latent_dims,))
+    x = decoder_input
     for nodes in reversed(hidden_layers):
-        x = tensorflow.keras.layers.Dense(nodes, activation='relu')(x)
-    
-    outputs = tensorflow.keras.layers.Dense(input_dims, activation='sigmoid')(x)
-    decoder = tensorflow.keras.Model(latent_inputs, outputs, name="decoder")
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    outputs = decoder(encoder(inputs)[0])
-    auto = tensorflow.keras.Model(inputs, outputs, name="autoencoder")
+    decoder = keras.models.Model(decoder_input, decoder_output, name="decoder")
 
-    def vae_loss(y_true, y_pred):
-        reconstruction_loss = tensorflow.keras.losses.binary_crossentropy(y_true, y_pred)
+    autoencoder_input = keras.layers.Input(shape=(input_dims,))
+    z, z_mean, z_log_sigma = encoder(autoencoder_input)
+    reconstructed = decoder(z)
+    auto = keras.models.Model(autoencoder_input, reconstructed, name="autoencoder")
+
+    def vae_loss(x, reconstructed_x):
+        """Documentation"""
+        reconstruction_loss = keras.losses.binary_crossentropy(x, reconstructed_x)
         reconstruction_loss *= input_dims
-        kl_loss = -0.5 * tensorflow.keras.backend.sum(1 + z_log_var - tensorflow.keras.backend.square(z_mean) - tensorflow.keras.backend.exp(z_log_var), axis=-1)
-        return tensorflow.keras.backend.mean(reconstruction_loss + kl_loss)
+        kl_loss = 1 + z_log_sigma - keras.backend.square(z_mean) - keras.backend.exp(z_log_sigma)
+        kl_loss = keras.backend.sum(kl_loss, axis=-1)
+        kl_loss *= -0.5
+        return keras.backend.mean(reconstruction_loss + kl_loss)
 
-    auto.compile(optimizer=tensorflow.keras.optimizers.Adam(), loss=vae_loss)
-    
+    auto.compile(optimizer=keras.optimizers.Adam(), loss=vae_loss)
+
     return encoder, decoder, auto
