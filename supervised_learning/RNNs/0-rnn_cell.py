54fc05b7435d5e5a12e5c89fe3ace3dce219f37f
iamseaniam <7222@holbertonstudents.com>
2024-10-29 12:57:44 -0500

    added public instace method to apply forward prop for one time step

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -22,13 +22,22 @@ class RNNCell:
         self.Wy = np.random.randn(h, o)
         self.by = np.zeros((1, o))
 
+    def softmax(self, z):
+        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
     def forward(self, h_prev, x_t):
         """ performs forward prop for one time step
-
         Args:
             h_prev numpy.ndarray:
                 contains previous hidden state, shape (m, h)
             x_t numpy.ndarray:
                 contains input data for the cell, shape (m, i)
         """
-        
+        concatenated = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(concatenated, self.Wh) + self.bh)
+
+        y_raw = np.dot(h_next, self.Wy) + self.by
+        y = self.softmax(y_raw)
+
+        return h_next, y
