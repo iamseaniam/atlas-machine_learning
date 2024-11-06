dd99a348d178125545828f101f3646468815503c
iamseaniam <sean.alexander613@gmail.com>
2024-11-06 15:44:38 -0600

    updated file

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -11,4 +11,11 @@ def rnn(rnn_cell, X, h_0):
         X (_type_): _description_
         h_0 (_type_): _description_
     """
-    pass
+    # retriving diminsions 
+    t, m, i = X.shape
+    _, h = h_0.shape
+
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
+
+    H[0] = h_0
