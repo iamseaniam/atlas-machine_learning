d5016c80284f404689bf58e173cb118f28dc66c0
iamseaniam <sean.alexander613@gmail.com>
2024-11-11 11:13:20 -0600

    final file

--- supervised_learning/RNNs/5-bi_forward.py
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -14,13 +14,21 @@ class BidirectionalCell:
             h (_type_): _description_
             o (_type_): _description_
         """
-        pass
+        self.Whf = np.random.randn(i + h, h)
+        self.bhf = np.zeros((1, h))
+
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+
+        self.Wy = np.random.randn(2 * h, o)
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """_summary_
-
-        Args:
-            h_prev (_type_): _description_
-            x_t (_type_): _description_
         """
-        pass
+        Calculates the forward hidden state for one time step.
+        """
+        concat_input = np.concatenate((h_prev, x_t), axis=1)
+
+        h_next = np.tanh(np.matmul(concat_input, self.Whf) + self.bhf)
+
+        return h_next
