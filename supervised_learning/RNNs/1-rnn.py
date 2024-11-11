f4d9dc2bc44036b16b1f92e5732216a79c8c42af
iamseaniam <sean.alexander613@gmail.com>
2024-11-11 10:51:43 -0600

    file 1 updated

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -4,18 +4,21 @@ import numpy as np
 
 
 def rnn(rnn_cell, X, h_0):
-    """ forward prop for a simple rnn
-
-    Args:
-        rnn_cell: is an instance of RNNCell that is used for forward prop
-        X (_type_): _description_
-        h_0 (_type_): _description_
     """
-    # retriving diminsions 
+    forward prop for a simple rnn
+    """
     t, m, i = X.shape
     _, h = h_0.shape
-
+    
     H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
 
     H[0] = h_0
+
+    for step in range(t):
+        h_next, y_next = rnn_cell.forward(H[step], X[step])
+        
+        H[step + 1] = h_next
+        Y[step] = y_next
+    
+    return H[1:], Y
