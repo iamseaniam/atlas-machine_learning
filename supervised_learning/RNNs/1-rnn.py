9a2ac3f06f9c7819be33d7ad86511392d73302ee
iamseaniam <sean.alexander613@gmail.com>
2024-11-11 11:00:15 -0600

    pycode style

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -9,7 +9,7 @@ def rnn(rnn_cell, X, h_0):
     """
     t, m, i = X.shape
     _, h = h_0.shape
-    
+
     H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
 
@@ -17,8 +17,8 @@ def rnn(rnn_cell, X, h_0):
 
     for step in range(t):
         h_next, y_next = rnn_cell.forward(H[step], X[step])
-        
+
         H[step + 1] = h_next
         Y[step] = y_next
-    
+
     return H[1:], Y
