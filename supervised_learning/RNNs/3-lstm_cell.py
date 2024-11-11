2a1decab0321f90063d0a48af865a70672941fa4
iamseaniam <sean.alexander613@gmail.com>
2024-11-11 11:06:07 -0600

    gfagghhhas

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -8,21 +8,52 @@ class LSTMCell:
     """
 
     def __init__(self, i, h, o):
-        """_summary_
-
-        Args:
-            i (_type_): _description_
-            h (_type_): _description_
-            o (_type_): _description_
         """
-        pass
+        Initializes the parameters of the LSTM cell.
+        """
+        self.Wf = np.random.randn(i + h, h)
+        self.bf = np.zeros((1, h))
 
-        def forward(self, h_prev, c_prev, x_t):
-            """_summary_
+        self.Wu = np.random.randn(i + h, h)
+        self.bu = np.zeros((1, h))
 
-            Args:
-                h_prev (_type_): _description_
-                c_prev (_type_): _description_
-                x_t (_type_): _description_
-            """
-            pass
+        self.Wc = np.random.randn(i + h, h)
+        self.bc = np.zeros((1, h))
+
+        self.Wo = np.random.randn(i + h, h)
+        self.bo = np.zeros((1, h))
+
+        self.Wy = np.random.randn(h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, c_prev, x_t):
+        """
+        Performs forward propagation for one time step in the LSTM cell.
+        """
+        concat_hx = np.concatenate((h_prev, x_t), axis=1)
+
+        f_t = self.sigmoid(np.dot(concat_hx, self.Wf) + self.bf)
+
+        u_t = self.sigmoid(np.dot(concat_hx, self.Wu) + self.bu)
+
+        c_tilde = np.tanh(np.dot(concat_hx, self.Wc) + self.bc)
+
+        c_next = f_t * c_prev + u_t * c_tilde
+
+        o_t = self.sigmoid(np.dot(concat_hx, self.Wo) + self.bo)
+
+        h_next = o_t * np.tanh(c_next)
+
+        y_linear = np.dot(h_next, self.Wy) + self.by
+        y = self.softmax(y_linear)
+
+        return h_next, c_next, y
+
+    def sigmoid(self, x):
+        """Helper method for the sigmoid activation function."""
+        return 1 / (1 + np.exp(-x))
+
+    def softmax(self, x):
+        """Helper method for the softmax activation function."""
+        exps = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exps / np.sum(exps, axis=1, keepdims=True)
