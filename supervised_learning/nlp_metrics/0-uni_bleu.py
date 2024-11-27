d227ee36a6857acb14c19c4df38b60a72372ca64
iamseaniam <sean.alexander613@gmail.com>
2024-11-27 15:22:36 -0600

    ZERO

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -5,4 +5,22 @@ import numpy as np
 
 def uni_bleu(references, sentence):
     """Documentation"""
-    pass
+    reference_words = {word for ref in references for word in ref}
+
+    matches = sum(1 for word in sentence if word in reference_words)
+
+    precision = matches / len(sentence) if len(sentence) > 0 else 0
+
+    reference_lengths = [len(ref) for ref in references]
+    closest_ref_len = min(
+        reference_lengths,
+        key=lambda ref_len: (abs(ref_len - len(sentence)), ref_len))
+    if len(sentence) > closest_ref_len:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = np.exp(
+            1 - closest_ref_len / len(sentence)) if len(sentence) > 0 else 0
+
+    bleu_score = brevity_penalty * precision
+
+    return bleu_score
