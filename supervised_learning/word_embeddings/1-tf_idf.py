be85baad144c1bece128b4246758d18f01a3ba2c
iamseaniam <sean.alexander613@gmail.com>
2024-11-27 15:04:31 -0600

    hrmmmmmmmmmgrkgrgrgrgrgrgr

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """dsijdsodjsd"""
 import gensim
+import numpy as np
 
 
 def tf_idf(sentences, vocab=None):
     """sodsdos"""
-    pass
+    tokenized_sentences = [sentence.lower().split() for sentence in sentences]
+
+    if vocab is None:
+        vocab = sorted(set(word for sentence in tokenized_sentences for word in sentence))
+    else:
+        vocab = sorted(vocab)
+
+    word_to_index = {word: i for i, word in enumerate(vocab)}
+
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+
+    for i, sentence in enumerate(tokenized_sentences):
+        for word in sentence:
+            if word in word_to_index:
+                embeddings[i, word_to_index[word]] += 1
+
+    return embeddings, vocab
