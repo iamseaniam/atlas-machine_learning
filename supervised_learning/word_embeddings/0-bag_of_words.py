8d4959ecb0308c3c7b1935066d436cd59cd6410a
iamseaniam <sean.alexander613@gmail.com>
2024-11-27 15:06:15 -0600

    gah this weather is not fun

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """dsijdsodjsd"""
 import gensim
+import numpy as np
 
 
 def bag_of_words(sentences, vocab=None):
     """sodsdos"""
-    pass
+    tokenized_sentences = [sentence.lower().split() for sentence in sentences]
+
+    if vocab is None:
+        vocab = sorted(set(word for sentence in tokenized_sentences for word in sentence))
+    else:
+        vocab = sorted(vocab)
+
+    word_to_index = {word: i for i, word in enumerate(vocab)}
+
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+
+    for i, sentence in enumerate(tokenized_sentences):
+        for word in sentence:
+            if word in word_to_index:
+                embeddings[i, word_to_index[word]] += 1
+
+    return embeddings, vocab
+
--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -6,20 +6,3 @@ import numpy as np
 
 def tf_idf(sentences, vocab=None):
     """sodsdos"""
-    tokenized_sentences = [sentence.lower().split() for sentence in sentences]
-
-    if vocab is None:
-        vocab = sorted(set(word for sentence in tokenized_sentences for word in sentence))
-    else:
-        vocab = sorted(vocab)
-
-    word_to_index = {word: i for i, word in enumerate(vocab)}
-
-    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-
-    for i, sentence in enumerate(tokenized_sentences):
-        for word in sentence:
-            if word in word_to_index:
-                embeddings[i, word_to_index[word]] += 1
-
-    return embeddings, vocab
