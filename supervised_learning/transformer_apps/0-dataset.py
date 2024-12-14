e7b77f4217dc36a1b84093d291ec695b775cfccd
iamseaniam <sean.alexander613@gmail.com>
2024-12-14 12:07:59 -0800

    updated file 0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -9,4 +9,24 @@ class Dataset:
 
     def __init__(self):
         """Documentation"""
-        pass
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+
+
+    def tokenizse_dataset(self, data):
+        """Documentation"""
+        tokenizer_pt = transformers.BertTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        tokenizer_en = transformers.BertTokenizer.from_pretrained("bert-base-uncased")
+
+        portuguese_sentenes = []
+        english_sentences = []
+        for pt, en in data.take(10000):
+            portuguese_sentenes.append(pt.numpy().decode('utf-8'))
+            english_sentences.append(en.numpy().decode('utf-8'))
+
+        tokenizer_pt = transformers.BertTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        tokenizer_en = transformers.BertTokenizer.from_pretrained("bert-base-uncased")
+            
+        return tokenizer_pt, tokenizer_en
