61c961ec09de3f72053cd5f6c4b637f289a9a294
iamseaniam <sean.alexander613@gmail.com>
2024-12-16 11:44:37 -0800

    weird error, testing what big boi checker says

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Documentation"""
-import transformers
+# https://huggingface.co/learn/nlp-course/en/chapter2/4
 import tensorflow_datasets as tfds
+import transformers
 import tensorflow as tf
 
 
@@ -9,13 +10,66 @@ class Dataset:
     """Documentation"""
 
     def __init__(self):
-        """documentation"""
-        pass
+        """Documentation"""
+
+        self.data_train = tfds.load(
+            "ted_hrlr_translate/pt_to_en", split="train", as_supervised=True
+        )
+        self.data_valid = tfds.load(
+            "ted_hrlr_translate/pt_to_en",
+            split="validation", as_supervised=True
+        )
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """Documentation"""
+
+        en_base = []
+        pt_base = []
+
+        for en, pt in data:
+            en_base.append(en.numpy().decode("utf-8"))
+            pt_base.append(pt.numpy().decode("utf-8"))
+
+        def en_iterator():
+            """documentation"""
+            yield from en_base
+
+        def pt_iterator():
+            """documentation"""
+            yield from pt_base
+
+        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased"
+        )
+        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
+            "bert-base-uncased"
+        )
+
+        vocab_size = 2**13
+
+        en_model_trained = tokenizer_en.train_new_from_iterator(
+            text_iterator=en_iterator(), vocab_size=vocab_size
+        )
+
+        pt_model_trained = tokenizer_pt.train_new_from_iterator(
+            text_iterator=pt_iterator(), vocab_size=vocab_size
+        )
+
+        return pt_model_trained, en_model_trained
 
     def encode(self, pt, en):
         """Documentation"""
-        pass
 
-    def tf_encode(self, pt, en):
-        """Documentation"""
-        pass
+        pt_tokens, en_tokens = tf.py_function(
+            func=self.encode,
+            inp=[pt, en],
+            Tout=[tf.int64, tf.int64]
+        )
+
+        pt_tokens.set_shape([None])
+        en_tokens.set_shape([None])
+
+        return pt_tokens, en_tokens
