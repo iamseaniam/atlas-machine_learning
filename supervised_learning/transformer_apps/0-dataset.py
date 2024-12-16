cf430bf1d3e58c8eafad4e12c17b6475f25473f5
iamseaniam <sean.alexander613@gmail.com>
2024-12-16 09:11:11 -0800

    no way the ")" is messing it up

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -11,12 +11,12 @@ class Dataset:
         """Documentation"""
 
         self.data_train = tfds.load(
-            'ted_hrlr_translate/pt_to_en', split='train', as_supervised=True
-            )
+            "ted_hrlr_translate/pt_to_en", split="train", as_supervised=True
+        )
         self.data_valid = tfds.load(
             'ted_hrlr_translate/pt_to_en', split='validation',
             as_supervised=True
-            )
+        )
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
