aaf77cd5c6979b07dfb9ac3efb799838b5797791
iamseaniam <sean.alexander613@gmail.com>
2024-12-16 11:04:09 -0800

    mehahha

--- supervised_learning/transformer_apps/1-dataset.py
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -3,6 +3,8 @@
 # https://huggingface.co/learn/nlp-course/en/chapter2/4
 import transformers
 import tensorflow_datasets as tfds
+import numpy as np
+import tensorflow as tf
 
 
 class Dataset:
@@ -19,7 +21,7 @@ class Dataset:
             split="validation", as_supervised=True
         )
 
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+        self.pt_tokenizer, self.en_tokenizer = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
@@ -40,20 +42,20 @@ class Dataset:
             """documentation"""
             yield from pt_base
 
-        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
+        pt_tokenizer = transformers.BertTokenizerFast.from_pretrained(
             "neuralmind/bert-base-portuguese-cased"
         )
-        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
+        en_tokenizer = transformers.BertTokenizerFast.from_pretrained(
             "bert-base-uncased"
         )
 
         vocab_size = 2**13
 
-        en_model_trained = tokenizer_en.train_new_from_iterator(
+        en_model_trained = en_tokenizer.train_new_from_iterator(
             text_iterator=en_iterator(), vocab_size=vocab_size
         )
 
-        pt_model_trained = tokenizer_pt.train_new_from_iterator(
+        pt_model_trained = pt_tokenizer.train_new_from_iterator(
             text_iterator=pt_iterator(), vocab_size=vocab_size
         )
 
@@ -61,7 +63,20 @@ class Dataset:
 
     def encode(self, pt, en):
         """Documentation"""
-        pt_tokens = self.tokenizer_pt.encode(pt.numpy().decode('utf-8'))
-        en_tokens = self.tokenizer_en.encode(en.numpy().decode('utf-8'))
+        pt_tokens = self.pt_tokenizer.encode(pt.numpy().decode('utf-8'))
+        en_tokens = self.en_tokenizer.encode(en.numpy().decode('utf-8'))
+
+        start_token_pt = self.pt_tokenizer.vocab_size
+        end_token_pt = self.pt_tokenizer.vocab_size + 1
+        
+        pt_tokens = [start_token_pt] + pt_tokens + [end_token_pt]
+        
+        start_token_en = self.en_tokenizer.vocab_size
+        end_token_en = self.en_tokenizer.vocab_size + 1
+        
+        en_tokens = [start_token_en] + en_tokens + [end_token_en]
+
+        pt_tokens = np.array(pt_tokens, dtype=np.int32)
+        en_tokens = np.array(en_tokens, dtype=np.int32)
 
         return pt_tokens, en_tokens
