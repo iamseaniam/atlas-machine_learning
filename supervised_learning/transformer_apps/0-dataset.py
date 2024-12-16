6ee777e52a87ec00ee516bf95050167f6bc29946
iamseaniam <sean.alexander613@gmail.com>
2024-12-16 09:25:02 -0800

    I DONT UNDERSTANDDDDDDDDDDDD

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -15,8 +15,8 @@ class Dataset:
             "ted_hrlr_translate/pt_to_en", split="train", as_supervised=True
         )
         self.data_valid = tfds.load(
-            'ted_hrlr_translate/pt_to_en', split='validation',
-            as_supervised=True
+            'ted_hrlr_translate/pt_to_en',
+            split='validation', as_supervised=True
         )
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
