dead0b00615e78caef6c458ea5207a5c7a5b2e83
iamseaniam <sean.alexander613@gmail.com>
2024-12-17 19:38:54 -0800

    this does not work

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 """Documentation"""
+import tensorflow as tf
+import tensorflow_hub as hub
+from transformers import BertTokenizer
 
 
 def question_answer(question, reference):
     """documentation"""
-    pass
+    try:
+        tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+        model = hub.load('https://tfhub.dev/see--/bert-uncased-tf2-qa/1')
+
+        inputs = tokenizer(question, reference, return_tensors='tf')
+
+        outputs = model([inputs['input_ids'], inputs['attention_mask']])
+        
+        start_logits, end_logits = outputs['start_logits'], outputs['end_logits']
+
+        start_index = tf.argmax(start_logits, axis=1).numpy()[0]
+        end_index = tf.argmax(end_logits, axis=1).numpy()[0]
+
+        tokens = inputs['input_ids'].numpy()[0][start_index:end_index + 1]
+        answer = tokenizer.decode(tokens)
+
+        return answer if answer else None
+    except Exception as e:
+        print(f"Error during question answering: {e}")
+        return None
