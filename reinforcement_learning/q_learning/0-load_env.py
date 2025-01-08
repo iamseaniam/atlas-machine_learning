423db22d1892873278d55a467435a516ff5b4fe9
iamseaniam <sean.alexander613@gmail.com>
2025-01-07 19:08:05 -0600

    a lot more simple than i was thinking it was (smh)

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """documentation"""
 import gymnasium as gym
+# from gymnasium.envs.toy_text im port frozen_lake
 
 # ! is it just making an enviroment?
 # ! all i am finding is stuff to do with the agent. 
 # CONFUSEd
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-    """_summary_
+    """Documentation"""
 
-    Args:
-        desc (_type_, optional): _description_. Defaults to None.
-        map_name (_type_, optional): _description_. Defaults to None.
-        is_slippery (bool, optional): _description_. Defaults to False.
-    """
+    # help(frozen_lake)
+    # ? i dont understant, just because i am using the module it then allowed me to make custom enviroment
+    evn = gym.make('FrozenLake-v1',
+                   desc=desc,
+                   map_name=map_name,
+                   is_slippery=True
+                   )
 
-    env = gym.make('FrozenLakeEnv')
-
-    ):
-        self.sise =
+    return evn
