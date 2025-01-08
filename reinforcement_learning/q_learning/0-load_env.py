6a51c1a62c9cb7f6342800276635ca958cf5660d
iamseaniam <sean.alexander613@gmail.com>
2025-01-07 19:17:38 -0600

    pycode validation and did not sync, would explain not finding the readme

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -4,8 +4,10 @@ import gymnasium as gym
 # from gymnasium.envs.toy_text im port frozen_lake
 
 # ! is it just making an enviroment?
-# ! all i am finding is stuff to do with the agent. 
+# ! all i am finding is stuff to do with the agent.
 # CONFUSEd
+
+
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """ Makes custotum enviroment for agent to use.
 
@@ -26,7 +28,7 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     # ? it then allowed me to make custom enviroment
 
     # just passing in the parameters to make function
-    evn = gym.make('FrozenLake-v1', # the module? Pretty sure
+    evn = gym.make('FrozenLake-v1',  # the module? Pretty sure
                    desc=desc, 
                    map_name=map_name,
                    is_slippery=is_slippery
