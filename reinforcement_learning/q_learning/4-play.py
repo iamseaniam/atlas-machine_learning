552509ffb097bf9afec3e1cea1c3d449c9407c46
iamseaniam <sean.alexander613@gmail.com>
2025-01-15 16:29:10 -0600

    I DO NOt KNOW WHY THIS ERROROROROR

---
+++ reinforcement_learning/q_learning/4-play.py
@@ -0,0 +1,21 @@
+def play(env, Q, max_steps=100):
+    """Plays an episode using the trained Q-table"""
+    state = env.reset()
+    total_reward = 0
+    rendered_outputs = []
+
+    for step in range(max_steps):
+        action = np.argmax(Q[state, :])
+        state, reward, done, _ = env.step(action)
+        total_reward += reward
+
+        # Render the current state of the environment
+        rendered_outputs.append(env.render())
+
+        if done:
+            break
+
+    # Ensure the final state is displayed
+    rendered_outputs.append(env.render())
+    
+    return total_reward, rendered_outputs
