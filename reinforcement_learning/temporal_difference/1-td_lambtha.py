4ceca0225eaee3ed1ab9dbe12ff36c4a224f838c
iamseaniam <sean.alexander613@gmail.com>
2025-01-29 15:14:51 -0600

    oops

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -3,7 +3,11 @@
 import numpy as np
 
 
-def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+def td_lambtha(env, V,
+               policy, lambtha,
+               episodes=5000, max_steps=100,
+               alpha=0.1, gamma=0.99
+               ):
     """documentation"""
     for episode in range(episodes):
         state, _ = env.reset()
