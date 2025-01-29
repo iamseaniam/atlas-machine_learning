a3d6fc610eaf8f44d70f78dea1303e4d9c4b7df0
iamseaniam <sean.alexander613@gmail.com>
2025-01-29 14:22:38 -0600

    i do not understannddddddddd

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -5,44 +5,43 @@ import numpy as np
 # 2:28pm 1/28/25 CODE does not look like expected output ):
 # 2:31 1/28/25 CODE just ouputs a 8x8 array of ones
 
+# * move all comments
+# ! TypeError: unsupported operand type(s) for %: 'tuple' and 'int'
+# # hopefully this will fix this error
+# range(start, stop, step)
+# starts loop from last index of episode_data
+# stops loop before reaching -1, end of index
+# makes loop step backwards
+# in mont carlo reutrns (G) are calculated backwards
+# update value function using incremtal forula
+# V(S_t) <- V(S_t) + a[G_t - V(S_t)]
+
 
 def monte_carlo(env, V, policy, episodes=5000,
                 max_steps=100, alpha=0.1, gamma=0.99
                 ):
     """Function that performs Monte Carlo algorithm"""
+    num_states = env.observation_space.n
+    returns = {s: [] for s in range(num_states)}
+
     for episodes in range(episodes):
         state = env.reset()
         episode_data = []  # storing (state, reward)
-
         for step in range(max_steps):
-            # ! TypeError: unsupported operand type(s) for %: 'tuple' and 'int'
-            # # hopefully this will fix this error
-            if isinstance(state, tuple):
-                state = state[0]
             action = policy(state)  # choosing action using policy
-
-            # ! ValueError: too many values to unpack (expected 4)
-            next_state, reward, done, _, _ = env.step(action)  # taking action
+            next_state, reward, done, _, _ = env.step(
+                action)  # taking action
             episode_data.append((state, reward))  # recording (state, reward)
-
+            state = next_state
             if done:
                 break
 
-            state = next_state
-
         G = 0  # initalize the return
-        # in mont carlo reutrns (G) are calculated backwards
-
-        # range(start, stop, step)
-        # starts loop from last index of episode_data
-        # stops loop before reaching -1, end of index
-        # makes loop step backwards
-        for t in range(len(episode_data) - 1, -1, -1):
-            state, reward = episode_data[t]
+        for g_state, reward in reversed(episode_data):
             G = gamma * G + reward  # compute return
 
-            # update value function using incremtal forula
-            # V(S_t) <- V(S_t) + a[G_t - V(S_t)]
-            V[state] += alpha * (G - V[state])
+            returns[g_state].append(G)  # store return
+
+            V[state] += alpha * np.mean(returns[g_state])
 
     return V
