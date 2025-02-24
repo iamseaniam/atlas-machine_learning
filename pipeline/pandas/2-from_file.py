cd84d97a3cafb92029901895ac107413485c719d
iamseaniam <sean.alexander613@gmail.com>
2025-02-24 14:33:19 -0600

    eeek added files

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 
 def from_file(filename, delimiter):
-    """no idea yet"""
+    """Function loads data from a file as a pandas DataFrame"""
     pd.DataFrame = pd.read_csv(filename, delimiter=delimiter)
     return pd.DataFrame
--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -3,9 +3,6 @@
 import pandas as pd
 
 
-import pandas as pd
-
-
 def from_numpy(array):
     """no idea yet"""
     return pd.DataFrame(array)
