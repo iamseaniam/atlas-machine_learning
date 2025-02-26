459ecc274dbeb19b3510a69359b83fc6c07f1ee1
iamseaniam <sean.alexander613@gmail.com>
2025-02-26 14:19:57 -0600

    NUMBEr four doneeeeee

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-"""Importing pandas for data analysis and manipulation"""
+"""Not importing anything. checker has the imports"""
 
 
 def array(df):
-    """no idea yet"""
-    pass
+    """Function grabs two columns and there last 10 rows
+    then converts it into a numpy.ndarray"""
+
+    # Grabbing named columns High, Close
+    grab_col = df[["High", "Close"]]
+
+    # in those columns this is grabbing the last ten rows
+    grab_rowcol = grab_col.tail(n=10)
+
+    # converts to numpy.ndarrary
+    np_arrary = grab_rowcol.to_numpy()
+
+    return np_arrary
