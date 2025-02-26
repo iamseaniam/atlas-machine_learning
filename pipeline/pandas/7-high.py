fb1f01a64387892b9e44ce44e0e0c3dfcbed114a
iamseaniam <sean.alexander613@gmail.com>
2025-02-26 15:26:59 -0600

    documentation for 7

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -3,7 +3,7 @@
 
 
 def high(df):
-    """no idea yet"""
+    """function sorts the column "High" data in descending order"""
     sort_desc = df.sort_values(by=['High'], ascending=False)
 
     return sort_desc
