5360b6cfc3a09b06f057f1d371a6b0eeb6c59cb9
iamseaniam <sean.alexander613@gmail.com>
2025-02-27 14:17:38 -0600

    i think i did it right

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -4,6 +4,6 @@
 
 def analyze(df):
     """function does computive descriptive statistics for all columns"""
-    stats = df[['Open', 'High', 'Low', 'Close',
-               'Volume_(BTC)', 'Weighted_Price']].describe()
-    return stats
+    df = df.drop(columns=['Timestamp'])
+    df = df.describe(include='all')
+    return df
