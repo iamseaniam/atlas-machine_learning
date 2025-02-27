66b954fbe114fac55905a149d0b6a70270adfc6b
iamseaniam <sean.alexander613@gmail.com>
2025-02-26 21:36:18 -0600

    ahaaaahaha, Reworked logic on all of it bascially

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -8,17 +8,14 @@ def fill(df):
     fills missing values for "High", "Low", "Open"
     Sets missing values "Volume_BTC", and "Volumn_Currency to 0"""
 
-    filled_df = df.drop('Weighted_Price', axis=1)
+    df = df.drop(columns=['Weighted_Price'])
 
-    filled_df.loc[:, ["Close"]] = filled_df.loc[:, ["Close"]].ffill()
+    df['Close'].fillna(method='ffill', inplace=True)
 
-    filled_df.loc["High"] = filled_df["High"].fillna(filled_df["Close"])
+    for col in ['High', 'Low', 'Open']:
+        df[col].fillna(df['Close'], inplace=True)
 
-    filled_df.loc["Low"] = filled_df["Low"].fillna(filled_df["Close"])
+    df[["Volume_(BTC)", "Volume_(Currency)"]] = df[[
+        "Volume_(BTC)", "Volume_(Currency)"]].fillna(0)
 
-    filled_df.loc["Open"] = filled_df["Open"].fillna(filled_df["Close"])
-
-    filled_df[["Volume_(BTC)", "Volume_(Currency)"]] = filled_df[[
-        "Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0)
-
-    return filled_df
+    return df
