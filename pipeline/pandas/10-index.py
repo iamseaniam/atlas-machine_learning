d228c154bcf7cc8224c089db0f5cce23103a2be4
iamseaniam <sean.alexander613@gmail.com>
2025-02-26 21:20:17 -0600

    10 should be done

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Importing pandas for data analysis and manipulation"""
+"""Checker Satisfaction"""
 
 
 def index(df):
-    """no idea yet"""
-    pass
+    """Function sets Timestamp as the index of the dataframe"""
+    index = df.set_index('Timestamp')
+
+    return index
