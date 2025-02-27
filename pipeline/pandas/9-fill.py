bd1133de7c869886e69312b0e02832acb90aff94
iamseaniam <sean.alexander613@gmail.com>
2025-02-26 21:14:29 -0600

    hmmm idk whats going on

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
-"""Importing pandas for data analysis and manipulation"""
+"""Checker Satisfaction"""
 
 
 def fill(df):
-    """no idea yet"""
-    pass
+    """This function removes the "Weighted_price" column
+    fills missing values in "Clo se" column
+    fills missing values for "High", "Low", "Open"
+    Sets missing values "Volume_BTC", and "Volumn_Currency to 0"""
+
+    filled_df = df.drop('Weighted_Price', axis=1)
+
+    filled_df.loc[:, ["Close"]] = filled_df.loc[: ["Close"]].ffill()
+
+    filled_df.loc["High"] = filled_df["High"].fillna(filled_df["Close"])
+
+    filled_df.loc["Low"] = filled_df["Low"].fillna(filled_df["Close"])
+
+    filled_df.loc["Open"] = filled_df["Open"].fillna(filled_df["Close"])
+
+    filled_df[["Volume_(BTC)", "Volume_(Currency)"]] = filled_df[[
+        "Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0)
+
+    return filled_df
