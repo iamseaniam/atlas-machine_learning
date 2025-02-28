cb6514d397a7592a7197f4aa90cf44e067c4317e
iamseaniam <sean.alexander613@gmail.com>
2025-02-27 22:25:01 -0600

    idk i dont understand it

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -5,26 +5,32 @@ import sys
 import time
 
 
-def get_user_location(api_url):
-    """Documentation"""
+def get_github_user_location(api_url):
     response = requests.get(api_url)
+
+    if response.status_code == 404:
+        print("Not found")
+        return
 
     if response.status_code == 403:
         reset_time = int(response.headers.get(
             "X-RateLimit-Reset", time.time()))
-        minutes_remaining = max(0, (reset_time - time.time()) // 60)
-        print(f"Reset in {int(minutes_remaining)} min")
-    elif response.status_code == 404:
-        print("Not found")
-    elif response.status_code == 200:
+        wait_minutes = (reset_time - time.time()) // 60
+        print(f"Reset in {int(wait_minutes)} min")
+        return
+
+    if response.status_code == 200:
         user_data = response.json()
-        print(user_data.get("location", "Location not available"))
+        location = user_data.get("location")
+        print(location if location else "Location not available")
     else:
-        print("Error fetching user data")
+        print(f"Error: Received status code {response.status_code}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./2-user_location.py <GitHub API URL>")
-    else:
-        get_user_location(sys.argv[1])
+        sys.exit(1)
+
+    api_url = sys.argv[1]
+    get_github_user_location(api_url)
