fd1ee16560b247125f701f7d93855211ce2c3fdd
iamseaniam <sean.alexander613@gmail.com>
2025-03-05 10:35:11 -0600

    redid question thurteeenn

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,9 +1,6 @@
 -- creates a table users
-DROP TABLE IF EXISTS 'users';
-
-CREATE TABLE users(
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
