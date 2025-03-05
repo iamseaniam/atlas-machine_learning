a374c2c64e635889ec2e75358cde492b5bba2946
iamseaniam <sean.alexander613@gmail.com>
2025-03-05 10:44:48 -0600

    FOURTEEN

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,7 +1,8 @@
 -- creates a table "users" 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country TEXT NOT NULL CHECK (country IN ('US', 'CO', 'TN')) DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
+
