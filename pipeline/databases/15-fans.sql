b9a49a34b76baa5a35b94fe3bce7ccbbcea61b26
iamseaniam <sean.alexander613@gmail.com>
2025-03-05 10:48:27 -0600

    hrmmmm hopefully canhandle more than 0

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,5 +1,6 @@
 -- ranks country origins of bands
-SELECT origin, SUM(nb_fans) AS total_fans
+SELECT origin, COALESCE(SUM(nb_fans), 0) AS total_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY total_fans DESC;
+
