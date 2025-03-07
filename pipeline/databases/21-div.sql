d145cfb5dc5455bc86ab8dd584442ef25b422f17
iamseaniam <sean.alexander613@gmail.com>
2025-03-07 11:08:40 -0600

    ):

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,15 @@
+-- creates a function SafeDiv
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT
+) RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    RETURN IF(b = 0, 0, a / b);
+END;
+
+//
+
+DELIMITER ;
